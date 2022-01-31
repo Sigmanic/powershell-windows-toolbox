d85b6d42ce44dc881c57b36ccc203e0f15f3949c
Sigmanic <71888803+Sigmanic@users.noreply.github.com>
2022-01-31 20:56:52 +0700

    Fixed Link Again

--- WindowsToolboxModify.ps1
+++ WindowsToolboxModify.ps1
@@ -2333,7 +2333,7 @@ Exit
 
 
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
-    $runasbox = {$i=(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Sigmanic/powershell-windows-toolbox/main/WindowsToolboxModify.ps');iex($i)}
+    $runasbox = {$i=(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Sigmanic/powershell-windows-toolbox/main/WindowsToolboxModify.ps1');iex($i)}
     Start-Process powershell $runasbox.ToString() -Verb RunAs 
     Exit
 }
